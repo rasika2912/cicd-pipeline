dbc60e54351685155680e0bea234dcd811a8ff42
rasika2912 <rasikadehankar2912@gmail.com>
2025-07-23 17:09:40 +0530

    new

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,10 @@
-from src.main import add
+from src.main import add ,subtract
 def test_add_function():
     assert add(2,3)==5
     assert add(0,0)==0
     assert add(5,5)==10
 
+def test_subtract_function():
+    assert add(3,3)==0
+    assert add(0,0)==0
+    assert add(10,5)==5
