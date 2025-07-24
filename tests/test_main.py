91bdb20ba718f023393c9fe602ceb0a76aea7224
rasika2912 <rasikadehankar2912@gmail.com>
2025-07-24 11:53:53 +0530

    Merged remote changes with local

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,15 @@
-from src.main import add ,subtract
+from src.main import add ,subtract,multiply
 def test_add_function():
     assert add(2,3)==5
     assert add(0,0)==0
     assert add(5,5)==10
 
 def test_subtract_function():
-    assert add(3,3)==0
-    assert add(0,0)==0
-    assert add(10,5)==5
+    assert subtract(3,3)==0
+    assert subtract(0,0)==0
+    assert subtract(10,5)==5
+
+def test_multiply_function():
+    assert multiply(3, 3) == 0
+    assert multiply(0, 0) == 0
+    assert multiply(10, 5) == 5
