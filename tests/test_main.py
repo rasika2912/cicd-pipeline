1581b2a28528ead67fa91328bc77242b70711ed9
rasika2912 <rasikadehankar2912@gmail.com>
2025-07-24 12:11:05 +0530

    today

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,6 +10,6 @@ def test_subtract_function():
     assert subtract(10,5)==5
 
 def test_multiply_function():
-    assert multiply(3, 3) == 0
+    assert multiply(3, 3) == 9
     assert multiply(0, 0) == 0
-    assert multiply(10, 5) == 5
+    assert multiply(10, 5) == 50
